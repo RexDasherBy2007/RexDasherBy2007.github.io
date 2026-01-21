d470ec404c4faf9964d494415047553eac408d30
RexDasherBy2007 <jhonderfd@gmail.com>
2026-01-20 21:06:41 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
-const aceptar =document.querySelector('#aceptar');
+const aceptar = document.querySelector('#aceptar');
 
-aceptar.addEventListener('click',function () {
-    alert('Sabía que dirías que siii💞💞💞')
+aceptar.addEventListener('click', function () {
+    alert('Sabía que dirías que siii💞💞💞');
 });
 
 const rechazar = document.querySelector('#rechazar');
 
-rechazar.addEventListener('mouseover',function() {
-    const randomX = parseInt(Math.random()*100);
-    const randomY = parseInt(Math.random()*100);
-    rechazar.style.setProperty('top', randomY+'%');
-    rechazar.style.setProperty('left', randomX+'%');
-    rechazar.style.setProperty('transform',`translate(-${randomX}%, -${randomY}%)`);
-    
-})
+// En lugar de mouseover, usamos click
+rechazar.addEventListener('click', function () {
+    const randomX = parseInt(Math.random() * 100);
+    const randomY = parseInt(Math.random() * 100);
+    rechazar.style.setProperty('top', randomY + '%');
+    rechazar.style.setProperty('left', randomX + '%');
+    rechazar.style.setProperty('transform', `translate(-${randomX}%, -${randomY}%)`);
+});
